3497010077b70a695fa1e8b1eb56a090ae83e114
nathael bellezza <nathael.bellezza@hotmail.com>
2023-04-29 11:57:23 -0300

    Agregue function al codigo

--- Entrega1/main.js
+++ Entrega1/main.js
@@ -1,22 +1,25 @@
 
-let nombre = prompt("Ingrese su nombre")
-let apellido = prompt("ingrese su apellido")
-alert("Bienvenido" + " " + nombre + " " + apellido)
+function sacarTurno() {
+    let nombre = prompt("Ingrese su nombre");
+    let apellido = prompt("Ingrese su apellido");
+    alert("Bienvenido " + nombre + " " + apellido);
 
-let turno = prompt("Desea sacar un turno? SI/NO")
+    let turno = prompt("¿Desea sacar un turno? SI/NO");
 
-if (turno.toLowerCase() === "no") {
-    alert("Gracias por visitar nuestra pagina.");
-}
+    if (turno.toLowerCase() === "no") {
+        alert("Gracias por visitar nuestra página.");
+    }
 
-if (turno.toLowerCase() === "si") {
-    let cantidadTurnos = parseInt(prompt("Ingrese la cantidad de turnos que desea sacar:"));
-    if (isNaN(cantidadTurnos) || cantidadTurnos < 1) {
-        alert("La cantidad de turnos ingresada es inválida.");
-    } else {
-        for (let i = 1; i <= cantidadTurnos; i++) {
-            let nombre = prompt("Ingrese su nombre para el turno " + i + ":");
-            alert("Turno " + i + ": " + nombre);
+    if (turno.toLowerCase() === "si") {
+        let cantidadTurnos = parseInt(prompt("Ingrese la cantidad de turnos que desea sacar:"));
+        if (isNaN(cantidadTurnos) || cantidadTurnos < 1) {
+            alert("La cantidad de turnos ingresada es inválida.");
+        } else {
+            for (let i = 1; i <= cantidadTurnos; i++) {
+                let nombreTurno = prompt("Ingrese su nombre para el turno " + i + ":");
+                alert("Turno " + i + ": " + nombreTurno);
+            }
         }
     }
 }
+sacarTurno();
